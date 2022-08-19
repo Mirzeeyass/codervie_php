d4027f1e376a0f7d39d1d900f5d51604a3294965
Abdullah Mirza <86431838+Mirzeeyass@users.noreply.github.com>
2022-08-19 08:07:22 -0700

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -18,7 +18,7 @@
         }else if($password == $confirm_password){
             $query = "INSERT INTO `users` (`sno`, `username`, `password`) VALUES (NULL, '".$username."', '".$password."')";
             $result = mysqli_query($connect, $query);
-            header ("location: http://localhost:8080/?signup=true");
+            header ("location: https://coderview.netlify.app/?signup=true");
         }else{
                 $incorrect = true;
             }
