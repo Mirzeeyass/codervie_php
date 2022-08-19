b4d625836ef8854aeb4b5465557af45e947f8c12
Abdullah Mirza <86431838+Mirzeeyass@users.noreply.github.com>
2022-08-19 07:30:59 -0700

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 session_start();
                 $_SESSION["login"] = true;
                 $_SESSION["username"] = $username;
-                header("location: http://localhost:8080?login=true");
+                header("location: http://localhost:8080?login=true&name=".$username);
             }else{
                 $incorrect = true;
             }
